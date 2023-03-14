09acc548fe30a8c2dfe585852d6c27abe0bb7f5a
bwogit <jeremie.sandot@hotmail.com>
2023-03-14 00:29:48 +0000

    Add data to the sheet from user Input

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sand')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("please enter sales data from the terminal")
+    print ("Data should be 6 stuff")
+    print("Example: 10,20,30,40,50,60\n")
 
-data = sales.get_all_values()
+    data_str = input("enter your data here:")
+    print(f"the data provided is {data_str}")
 
-print(data)
+get_sales_data()   
